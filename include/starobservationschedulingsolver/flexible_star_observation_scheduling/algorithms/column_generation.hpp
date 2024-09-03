31e625263239ad951e0e5427c6c3b221a27541ee
Florian Fontan <dev@florian-fontan.fr>
2024-09-03 22:06:32 +0200

    Move comments

--- include/starobservationschedulingsolver/flexible_star_observation_scheduling/algorithms/column_generation.hpp
+++ include/starobservationschedulingsolver/flexible_star_observation_scheduling/algorithms/column_generation.hpp
@@ -1,37 +1,3 @@
-/**
- * Dantzig-Wolfe decomposition.
- *
- * The linear programming formulation of the problem based on Dantzig–Wolfe
- * decomposition is written as follows:
- *
- * Variables:
- * - yᵢᵏ ∈ {0, 1} representing a feasible schedule of night i.
- *   yᵢᵏ = 1 iff the corresponding schedule is selected for night i.
- *   xⱼᵢᵏ = 1 iff yᵢᵏ contains target j, otherwise 0.
- *
- * Program:
- *
- * max ∑ᵢ ∑ₖ (∑ⱼ wⱼ xⱼᵢᵏ) yᵢᵏ
- *                                      Note that (∑ⱼ wⱼ xⱼᵢᵏ) is a constant.
- *
- * 0 <= ∑ₖ yᵢᵏ <= 1        for all nights i
- *                         (not more than 1 schedule selected for each night)
- *                                                         Dual variables: uᵢ
- * 0 <= ∑ₖ xⱼᵢᵏ yᵢᵏ <= 1   for all targets j
- *                                        (each target selected at most once)
- *                                                         Dual variables: vⱼ
- *
- * The pricing problem consists in finding a variable of positive reduced cost.
- * The reduced cost of a variable yᵢᵏ is given by:
- * rc(yᵢᵏ) = ∑ⱼ wⱼ xⱼᵢᵏ - uᵢ - ∑ⱼ xⱼᵢᵏ vⱼ
- *         = ∑ⱼ (wⱼ - vⱼ) xⱼᵢᵏ - uᵢ
- *
- * Therefore, finding a variable of maximum reduced cost reduces to solving m
- * Single Night Star Observation Scheduling Problems with targets with profit
- * (wⱼ - vⱼ).
- *
- */
-
 #pragma once
 
 #include "starobservationschedulingsolver/flexible_star_observation_scheduling/solution.hpp"
--- src/star_observation_scheduling/algorithms/column_generation.cpp
+++ src/star_observation_scheduling/algorithms/column_generation.cpp
@@ -1,3 +1,40 @@
+/**
+ * Star observation scheduling problem
+ *
+ * Problem description:
+ * See https://github.com/fontanf/orproblems/blob/main/orproblems/starobservationscheduling.hpp
+ *
+ * The linear programming formulation of the problem based on Dantzig–Wolfe
+ * decomposition is written as follows:
+ *
+ * Variables:
+ * - yᵢᵏ ∈ {0, 1} representing a feasible schedule of night i.
+ *   yᵢᵏ = 1 iff the corresponding schedule is selected for night i.
+ *   xⱼᵢᵏ = 1 iff yᵢᵏ contains target j, otherwise 0.
+ *
+ * Program:
+ *
+ * max ∑ᵢ ∑ₖ (∑ⱼ wⱼ xⱼᵢᵏ) yᵢᵏ
+ *                                      Note that (∑ⱼ wⱼ xⱼᵢᵏ) is a constant.
+ *
+ * 0 <= ∑ₖ yᵢᵏ <= 1        for all nights i
+ *                         (not more than 1 schedule selected for each night)
+ *                                                         Dual variables: uᵢ
+ * 0 <= ∑ₖ xⱼᵢᵏ yᵢᵏ <= 1   for all targets j
+ *                                        (each target selected at most once)
+ *                                                         Dual variables: vⱼ
+ *
+ * The pricing problem consists in finding a variable of positive reduced cost.
+ * The reduced cost of a variable yᵢᵏ is given by:
+ * rc(yᵢᵏ) = ∑ⱼ wⱼ xⱼᵢᵏ - uᵢ - ∑ⱼ xⱼᵢᵏ vⱼ
+ *         = ∑ⱼ (wⱼ - vⱼ) xⱼᵢᵏ - uᵢ
+ *
+ * Therefore, finding a variable of maximum reduced cost reduces to solving m
+ * Single Night Star Observation Scheduling Problems with targets with profit
+ * (wⱼ - vⱼ).
+ *
+ */
+
 #include "starobservationschedulingsolver/star_observation_scheduling/algorithms/column_generation.hpp"
 
 #include "starobservationschedulingsolver/star_observation_scheduling/algorithm_formatter.hpp"
